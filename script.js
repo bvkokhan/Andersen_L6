ec0c83208fd32d8b5b55176abc33d31ba44c6aa4
Bogdan <bv.kokhan@gmail.com>
2021-04-04 23:27:01 +0300

    6 homework, fixed constructor and setters and some gramatic mistakes

--- script.js
+++ script.js
@@ -10,25 +10,59 @@ class Car {
     #mileage;
 
     constructor(brand, model, yearOfManufacturing, maxSpeed, maxFuelVolume, fuelConsumption, currentFuelVolume = 0, isStarted = false, mileage = 0) {
-        this.#brand = brand;
-        this.#model = model;
-        this.#yearOfManufacturing = yearOfManufacturing;
-        this.#maxSpeed = maxSpeed;
-        this.#maxFuelVolume = maxFuelVolume;
-        this.#fuelConsumption = fuelConsumption;
-        this.#currentFuelVolume = currentFuelVolume;
-        this.#isStarted = isStarted;
-        this.#mileage = mileage;
-
-        
-    }
-
-    start(){
-        if(this.#isStarted = false){
-            this.#isStarted = true
-            console.log(this.#isStarted)
+        if ( typeof brand === 'string' && brand.length < 50){
+            this.#brand = brand;
+        } else {
+            throw new Error( 'Wrong Brand value')
         }
+
+        if ( typeof model === 'string' && model.length < 50){
+            this.#model = model;
+        } else {
+            throw new Error( 'Wrong Model value')
+        }
+
+        if (yearOfManufacturing <= 2021 && yearOfManufacturing >= 1900 && Number.isInteger(yearOfManufacturing)){
+            this.#yearOfManufacturing = yearOfManufacturing;
+        } else {
+            throw new Error( 'Wrong Year Of Manufacturing value')
+        }
+
+        if(typeof maxSpeed === "number" && maxSpeed > 100 && maxSpeed < 300) {
+            this.#maxSpeed = maxSpeed;
+        } else {
+            throw new Error( 'Wrong Max Speed value')
+        }
+
+        if(typeof maxFuelVolume === "number" && maxFuelVolume > 5 && maxFuelVolume < 20){
+            this.#maxFuelVolume = maxFuelVolume;
+        } else {
+            throw new Error( 'Wrong Max Fuel Volume value')
+        }
+
+        if(typeof fuelConsumption === "number") {
+            this.#fuelConsumption = fuelConsumption;
+        } else {
+            throw new Error('Wrong Fuel Consumption value')
+        }
+
+        if (typeof currentFuelVolume === "number" && currentFuelVolume <= maxFuelVolume && currentFuelVolume >= 0){
+            this.#currentFuelVolume = currentFuelVolume;
+        } else {
+            throw new Error('Wrong Current Fuel Volume value')
+        }
+
+        if (typeof isStarted === 'boolean'){
+            this.#isStarted = isStarted; 
+        } else {
+            throw new Error('Wrong value of is Started')
+        }
+
+        if (typeof mileage === 'number')
+        this.#mileage = mileage;
     }
+
+
     get brand() {
         return this.#brand
     }
@@ -37,7 +71,7 @@ class Car {
         if(typeof value === "string" && value.length < 50){
             this.#brand = value;
         } else{
-            throw new Error('xlkflke')
+            throw new Error('Wrong value of brand')
         }
     }
 
@@ -47,6 +81,8 @@ class Car {
     set model(value){
         if(typeof value === "string" && value.length < 50){
             this.#model = value;
+        } else {
+            throw new Error('Wrong value of model')
         }
     }
 
@@ -57,7 +93,7 @@ class Car {
         if(typeof value === "number" && value <= 2021 && value >= 1900){
             this.#yearOfManufacturing = value;
         }else {
-            console.log('err')
+            throw new Error('Wrong value of yearOfManufacturing')
         }
     }
 
@@ -67,6 +103,8 @@ class Car {
     set maxSpeed(value){
         if(typeof value === "number" && value > 100 && value < 300) {
             this.#maxSpeed = value
+        } else {
+            throw new Error('Wrong value of maxSpeed')
         }
     }
 
@@ -76,6 +114,8 @@ class Car {
     set maxFuelVolume(value){
         if(typeof value === "number" && value > 5 && value < 20){
             this.#maxFuelVolume = value;
+        } else{
+            throw new Error('Wrong value of maxFuelVolume')
         }
     }
 
@@ -85,6 +125,8 @@ class Car {
     set fuelConsumption(value){
         if(typeof value === "Number") {
             this.#fuelConsumption = value;
+        } else {
+            throw new Error('Wrong value of fuelConsumption')
         }
     }
 
@@ -92,29 +134,32 @@ class Car {
         return `${this.#currentFuelVolume} л`
     }
 
-
     get isStarted(){
         return this.#isStarted;
     }
-
 
     get mileage(){
         return `${this.#mileage} км`
     }
 
-
-    
-    
+    start(){
+        if(this.#isStarted){
+            throw new Error('Машина уже заведена')
+        } else{
+            this.#isStarted = !this.#isStarted
+        }
+    }
 
     shutDownEngine(){
-        if(this.#isStarted = false){
+        if(this.#isStarted){
+            this.#isStarted = !this.#isStarted;
+        } else{
             throw new Error('Машина ещё не заведена');
         }
-        this.#isStarted = false;
     }
 
     fillUpGasTank(fuelVolume){
-        if(typeof fuelVolume !=='Number' || fuelVolume <= 0){
+        if(typeof fuelVolume !=='number' || fuelVolume <= 0){
             throw new Error('Неверное количество топлива для заправки');
         }
         if (fuelVolume + this.#currentFuelVolume > this.#maxFuelVolume) {
@@ -122,18 +167,19 @@ class Car {
         }
         this.#currentFuelVolume += fuelVolume;
     }
+
     drive(speed, hours){
-        if(typeof speed !=='Number' || speed <= 0){
+        if(typeof speed !=='number' || speed <= 0){
             throw new Error('Неверная скорость');
         }
-        if(typeof hours !=='Number' || hours <= 0){
+        if(typeof hours !=='number' || hours <= 0){
             throw new Error('Неверное количество часов');
         }
         
         if (speed > this.#maxSpeed){
             throw new Error('Машина не может ехать так быстро')
         }
-        if (this.#isStarted = false){
+        if (!this.#isStarted){
             throw new Error('Машина должна быть заведена, чтобы ехать');
         }
         if(hours * this.#fuelConsumption > this.#currentFuelVolume){
@@ -143,4 +189,3 @@ class Car {
         this.#currentFuelVolume -=  this.#fuelConsumption * hours;
     }
 }
-
